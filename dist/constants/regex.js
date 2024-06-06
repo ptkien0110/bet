10f9b173b94d464753757ca0dd71946981881cf5
ptkien0110 <ptkien0110@gmail.com>
2024-06-06 11:29:00 +0700

    k

--- dist/constants/regex.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REGEX_PHONE_NUMBER = void 0;
-exports.REGEX_PHONE_NUMBER = /(03|05|07|08|09|01[2|6|8|9])+([0-9]{8})\b/;
